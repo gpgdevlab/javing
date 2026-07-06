5d6d40267cf5df275447e886f6d0d7e785daa619
Pimentel <gabriel.pimentel1110@gmail.com>
2026-07-06 13:14:52 -0300

    Add constructor

--- yaaa/src/ExercicioQuinze/Main.java
+++ yaaa/src/ExercicioQuinze/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		Yay objetoPessoa = new Yay();
+
+		Pessoa objetoPessoa = new Pessoa(70.0f, 1.70f);
 		Scanner leitorScanner = new Scanner(System.in);
 		
 		System.out.print("Digite o peso (ex: 70,5): ");
-		objetoPessoa.peso = leitorScanner.nextFloat();
+		objetoPessoa.setPeso(leitorScanner.nextFloat());
 		
 		System.out.print("Digite a altura (ex: 1,75): ");
-		objetoPessoa.altura = leitorScanner.nextFloat();
+		objetoPessoa.setAltura(leitorScanner.nextFloat());
 		
 		System.out.println("O IMC é: " + objetoPessoa.calcularIMC());
 		
