2d561dcb35a09adfd31be2a76d46f48d5ae78fe8
Pimentel <79454863+gpgdevlab@users.noreply.github.com>
2026-07-06 19:40:17 -0300

    Add input validation loop for weight and height
    Refactor input handling to use a loop with exception handling.

--- yaaa/src/ExercicioQuinze/Main.java
+++ yaaa/src/ExercicioQuinze/Main.java
@@ -6,16 +6,26 @@ public class Main {
 	public static void main(String[] args) {
 
 		Pessoa objetoPessoa = new Pessoa(70.0f, 1.70f);
-		Scanner leitorScanner = new Scanner(System.in);
+
+		boolean x = true;
+
+		while (x) {
+			try (Scanner leitorScanner = new Scanner(System.in)) {
+			System.out.print("Digite o peso (ex: 70,5): ");
+			objetoPessoa.setPeso(leitorScanner.nextFloat());
+
+			System.out.print("Digite a altura (ex: 1,75): ");
+			objetoPessoa.setAltura(leitorScanner.nextFloat());
+
+			System.out.println("O IMC é: " + objetoPessoa.calcularIMC());
+
+			x = false;
+			
+			leitorScanner.close();
+		} catch (Exception e) {
+     		 System.out.println("Something went wrong.");
+		}
+		}
 		
-		System.out.print("Digite o peso (ex: 70,5): ");
-		objetoPessoa.setPeso(leitorScanner.nextFloat());
-		
-		System.out.print("Digite a altura (ex: 1,75): ");
-		objetoPessoa.setAltura(leitorScanner.nextFloat());
-		
-		System.out.println("O IMC é: " + objetoPessoa.calcularIMC());
-		
-		leitorScanner.close();	
 	}
 }
